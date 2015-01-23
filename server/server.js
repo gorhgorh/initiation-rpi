ed9974fb19f13d8630c49ac960457317f739368b
Jérôme Loï <gorhgorh@gmail.com>
2015-01-23 22:56:29 +0100

    removed socket.io

--- server/server.js
+++ server/server.js
@@ -57,13 +57,3 @@ function setup() {
 }
 
 
-/**
- * socket.io
- */
-
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-});
